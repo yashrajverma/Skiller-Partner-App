849985412528f5cc62868b44a4ed40eb049fbff5
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-08-19 17:33:45 +0530

    Fragments for Dashboard Activity

---
+++ app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/CompletedFragment.java
@@ -0,0 +1,21 @@
+package com.yashraj.skillerpartnerapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.yashraj.skillerpartnerapp.R;
+
+
+public class CompletedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_completed, container, false);
+    }
+}
