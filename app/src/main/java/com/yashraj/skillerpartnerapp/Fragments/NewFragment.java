a9d0fc7dd8ff60a78fea9543b12d5ecbb417e733
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-10-14 17:14:53 +0530

    Fragment Activity

--- app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/NewFragment.java
+++ app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/NewFragment.java
@@ -6,17 +6,37 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.yashraj.skillerpartnerapp.Adapter.NewTaskAdapter;
+import com.yashraj.skillerpartnerapp.Model.NewTask;
 import com.yashraj.skillerpartnerapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class NewFragment extends Fragment {
+    private RecyclerView newTaskRecyclerView;
+    private NewTaskAdapter newTaskAdapter;
+    private List<NewTask> taskList;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new, container, false);
+        View view = inflater.inflate(R.layout.fragment_new, container, false);
+        newTaskRecyclerView = view.findViewById(R.id.new_task);
+        newTaskRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        newTaskRecyclerView.setLayoutManager(linearLayoutManager);
+        taskList = new ArrayList<>();
+        newTaskAdapter = new NewTaskAdapter(getContext(), taskList);
+        newTaskRecyclerView.setAdapter(newTaskAdapter);
+
+
+        return view;
     }
 }
