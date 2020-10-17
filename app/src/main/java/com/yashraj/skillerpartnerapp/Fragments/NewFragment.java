59c857a7a1c8d9c472cf5173556680eab791163d
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-10-17 14:21:53 +0530

    Fragments

--- app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/NewFragment.java
+++ app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/NewFragment.java
@@ -5,10 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.yashraj.skillerpartnerapp.Adapter.NewTaskAdapter;
 import com.yashraj.skillerpartnerapp.Model.NewTask;
 import com.yashraj.skillerpartnerapp.R;
@@ -18,9 +24,9 @@ import java.util.List;
 
 
 public class NewFragment extends Fragment {
-    private RecyclerView newTaskRecyclerView;
-    private NewTaskAdapter newTaskAdapter;
-    private List<NewTask> taskList;
+    RecyclerView newTaskRecyclerView;
+    NewTaskAdapter newTaskAdapter;
+    List<NewTask> taskList;
 
 
     @Override
@@ -29,14 +35,39 @@ public class NewFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_new, container, false);
         newTaskRecyclerView = view.findViewById(R.id.new_task);
-        newTaskRecyclerView.setHasFixedSize(true);
+//        newTaskRecyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         newTaskRecyclerView.setLayoutManager(linearLayoutManager);
         taskList = new ArrayList<>();
         newTaskAdapter = new NewTaskAdapter(getContext(), taskList);
         newTaskRecyclerView.setAdapter(newTaskAdapter);
+        readTasks();
 
 
         return view;
+    }
+
+    private void readTasks() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("NewTask");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                taskList.clear();
+
+                for (DataSnapshot datasnapshot : snapshot.getChildren()) {
+                    NewTask tasks = datasnapshot.getValue(NewTask.class);
+                    taskList.add(tasks);
+
+                }
+                newTaskAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/OngoingFragment.java
+++ app/src/main/java/com/yashraj/skillerpartnerapp/Fragments/OngoingFragment.java
@@ -6,60 +6,38 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.yashraj.skillerpartnerapp.Adapter.OngoingTaskAdapter;
+import com.yashraj.skillerpartnerapp.Model.OngoingTask;
 import com.yashraj.skillerpartnerapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OngoingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class OngoingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OngoingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OngoingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OngoingFragment newInstance(String param1, String param2) {
-        OngoingFragment fragment = new OngoingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RecyclerView onGoingTask;
+    ArrayList<OngoingTask> dataHolder;
+    OngoingTaskAdapter taskAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ongoing, container, false);
+        View view = inflater.inflate(R.layout.fragment_ongoing, container, false);
+        onGoingTask = view.findViewById(R.id.ongoing_task);
+        onGoingTask.setLayoutManager(new LinearLayoutManager(getContext()));
+        dataHolder = new ArrayList<OngoingTask>();
+
+        OngoingTask ob1 = new OngoingTask("Panchvati,Agra", "23-oct-2020");
+        dataHolder.add(ob1);
+        OngoingTask ob2 = new OngoingTask("Indrapuram", "15-oct-2020");
+        dataHolder.add(ob2);
+        taskAdapter = new OngoingTaskAdapter(dataHolder, getContext());
+        onGoingTask.setAdapter(taskAdapter);
+
+        return view;
     }
 }
